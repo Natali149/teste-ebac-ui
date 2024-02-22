ef2478dce25a1f3a41129d2696b1b89b81466377
Natali Magalhaes <natalimagalhaes149@gmail.com>
2024-02-22 19:05:49 -0300

    Alterações finais mod 12

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress" />
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
-    afterEach(() => {
-        cy.screenshot()
-    });
+    //  afterEach(() => {
+    //    cy.screenshot()
+    // });
 
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('natali.teste@teste.com.br')
@@ -26,11 +27,32 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error').should('exist')
     });
 
-    it('Deve exibir uma mensagem de erro ao inserir uma sssssenha invalida', () => {
+    it('Deve exibir uma mensagem de erro ao inserir uma senha invalida', () => {
         cy.get('#username').type('natali.teste@teste.com.br')
         cy.get('#password').type('teste@124')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail natali.teste@teste.com.br está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error').should('exist')
+    });
+    it('Deve fazer logincom sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, natali.teste (não é natali.teste? Sair)')
+    });
+    it('Deve fazer logincom sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, { log: false })
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, natali.teste (não é natali.teste? Sair)')
+
+        })
+
+    });
+    it.only('Deve fazer login com sucesso - Usando Comandos Customizados', () => {
+        cy.login('natali.teste@teste.com.br', 'teste@1234')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, natali.teste (não é natali.teste? Sair)')
+
     });
 })
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,44 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
+
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        produtosPage.visitarUrl()
     });
 
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ariel Roll Sleeve Sweatshirt')
-            .click()
-
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Aero Daily Fitness Tee'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('L', 'Red', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + '  × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })
+    });
+
 });
